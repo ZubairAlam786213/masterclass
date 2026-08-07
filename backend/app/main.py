291f18ee3bbd00d19063c47897898f73faf10747
Zubair Alam <zubairalam786213@gmail.com>
2026-08-07 17:37:47 +0530

    SMTP

---
+++ backend/app/main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+
+
+@app.get("/")
+def root():
+    return { "hello" : "Welcome to the masterclass" }
+
+
+
+@app.get("/name/{user_name}")
+def info(user_name:str):
+    return { "hello" : f"Welcome to the masterclass {user_name}" }
+
+
+
+from pydantic import BaseModel,EmailStr
+
+class UserData(BaseModel):
+    name : str
+    email : EmailStr
+
+
+@app.post("/user")
+def user_data(data:UserData):
+    return {"result" : f"hello this is post mapping we got the name {data.name}"}
+
+
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+from fastapi import FastAPI, HTTPException, status
+from email.mime.text import MIMEText
+import smtplib
+import os
+class ContactRequest(BaseModel):
+    name: str
+    email: EmailStr
+    message: str
+
+@app.post("/api/contact", status_code=status.HTTP_200_OK)
+def send_portfolio_email(payload: ContactRequest):
+    # Retrieve securely injected variables from Render's dashboard environment
+    smtp_host = os.environ.get("SMTP_HOST")
+    smtp_user = os.environ.get("SMTP_USER")
+    smtp_pass = os.environ.get("SMTP_PASS")
+
+    target_email = os.environ.get("MY_PERSONAL_EMAIL")
+    print({
+        "SMTP_HOST": os.getenv("SMTP_HOST"),
+        "SMTP_USER": os.getenv("SMTP_USER"),
+        "SMTP_PASS": "***" if os.getenv("SMTP_PASS") else None,
+        "MY_PERSONAL_EMAIL": os.getenv("MY_PERSONAL_EMAIL"),
+        })
+    if not all([smtp_host, smtp_user, smtp_pass, target_email]):
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="SMTP server environment variables are missing on the host platform."
+        )
+
+    # Format email formatting strings
+    body_content = f"New Portfolio Message!\n\nName: {payload.name}\nEmail: {payload.email}\n\nMessage:\n{payload.message}"
+    
+    msg = MIMEText(body_content)
+    msg["Subject"] = f"💼 Portfolio: Message from {payload.name}"
+    msg["From"] = smtp_user
+    msg["To"] = target_email
+
+    try:
+        # Establish secure SSL pipeline via Port 465 (Safe for Render)
+        with smtplib.SMTP_SSL(smtp_host, 465,timeout=10) as server:
+            server.login(smtp_user, smtp_pass)
+            server.send_message(msg)
+        print("connected fine")
+        return {"success": True, "message": "Your message was transmitted successfully!"}
+        
+    except smtplib.SMTPAuthenticationError:
+        raise HTTPException(status_code=401, detail="Authentication failed. Check your SMTP user/password.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to route email: {str(e)}")
